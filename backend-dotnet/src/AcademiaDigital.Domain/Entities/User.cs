8551a48865bda7bb4907dd54331b7ee565b01b1c
sergioortiz17 <sergio.ortiz.mendoza@mi.unc.edu.ar>
2026-05-02 23:02:52 -0300

    feat/add-user-lastname-to-migrations-test

--- backend-dotnet/src/AcademiaDigital.Domain/Entities/User.cs
+++ backend-dotnet/src/AcademiaDigital.Domain/Entities/User.cs
@@ -4,6 +4,7 @@ public class User
 {
     public long Id { get; set; }
     public string Username { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public bool IsActive { get; set; } = true;
